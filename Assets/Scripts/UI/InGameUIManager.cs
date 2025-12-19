8c51cd5a13d1415e811b5e335678365c99599a1d
Hash2O <archeosam@gmail.com>
2025-12-19 12:28:37 +0100

    Game Menu de base : activable via bouton de la manette droite. Permet d'afficher/cacher les noms et les icones des ingrédients dans la scène, de lancer ou arrêter le tuto audio, de régler le volume de la musique d'ambiance, et de quitter proprement le jeu.

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -1,22 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using TMPro;
 
 public class InGameUIManager : MonoBehaviour
 {
-
+    [SerializeField] private GameObject inGameMenu;
     [SerializeField] private List<Canvas> inGameUICanvas;
+    [SerializeField] private TextMeshProUGUI inGameUICanvasButtonText;
 
     [SerializeField] private PlayableDirector director;
+    [SerializeField] private TextMeshProUGUI directorButtonText;
 
+    private bool isInGameMenuActive;
     private bool isInGameUIActive;
     private bool isNarrativeStoryActive;
 
 
     private void Start()
     {
+        isInGameMenuActive = false;
         isInGameUIActive = true;
         isNarrativeStoryActive = true;
+
+        inGameUICanvasButtonText.text = "Show";
+        directorButtonText.text = "Stop";
 
         ToggleInGameUI();
     }
@@ -29,6 +37,7 @@ public class InGameUIManager : MonoBehaviour
                  item.gameObject.SetActive(false);
             }
             isInGameUIActive = false;
+            inGameUICanvasButtonText.text = "Show";
         }
         else
         {
@@ -37,6 +46,7 @@ public class InGameUIManager : MonoBehaviour
                 item.gameObject.SetActive(true);
             }
             isInGameUIActive = true;
+            inGameUICanvasButtonText.text = "Hide";
         }
     }
 
@@ -46,11 +56,32 @@ public class InGameUIManager : MonoBehaviour
         {
             director.gameObject.SetActive(false);
             isNarrativeStoryActive = false;
+            directorButtonText.text = "Start";
         }
         else if (isNarrativeStoryActive == false)
         {
             director.gameObject.SetActive(true);
             isNarrativeStoryActive = true;
+            directorButtonText.text = "Stop";
         }
+    }
+
+    public void ToggleInGameMenu()
+    {
+        if(isInGameMenuActive == false)
+        {
+            inGameMenu.gameObject.SetActive(true);
+            isInGameMenuActive = true;
+        }
+        else if(isInGameMenuActive == true)
+        {
+            inGameMenu.gameObject.SetActive(false);
+            isInGameMenuActive = false;
+        }
+    }
+
+    public void LeaveKitchen()
+    {
+        if(GameManager.Instance != null) GameManager.Instance.QuitGame();
     }
 }
